b8d941b17a51dd44ec7a5a2592e42a65302fabdd
tristan-genspark <tristan.stringfellow@genspark.net>
2022-07-21 21:49:35 -0500

    Changes to the scripts

--- Build-Jenkins/setup.sh
+++ Build-Jenkins/setup.sh
@@ -3,25 +3,33 @@
 # create the namespace for the container
 kubectl create namespace management-ns
 
-# print out the
-kubectl run jenkins-app --image=jenkins --port=8080 --dry-run=client -n management-ns -o yaml > deploy.yaml
+# create the yaml required to make a POD with jenkins running in it
+kubectl run jenkins-app --image=jenkins:lts --port=8080 --dry-run=client -n management-ns -o yaml > deploy.yaml
 
 kubectl apply -f deploy.yaml -n management-ns
 
 # view deployments
 kubectl get deployments -n management-ns
 
-kubectl expose deployment.apps/jenkins-app --type=NodePort --name=jenkins-access
-
 # unable to expose it because it says it cant find it
-kubectl expose pods jenkins-app --port=8080 --target-port=8080 --type=NodePort --name=jenkins-access
+kubectl expose po jenkins-app --port=8080 --target-port=8080 --type=NodePort --name=jenkins-access
 
 kubectl get pods -n management-ns
-
-# export CLUSTER_IP=$(kubectl get services/nfs-server -o go-template='{{(index.spec.clusterIP)}}');echo CLUSTER_IP=$CLUSTER_IP
 
 # for testing - quickly delete created pods
 kubectl delete --all services
 kubectl delete --all deployments
 kubectl delete --all pods
 rm deploy.yaml
+
+
+
+
+# Get the IP of the service   try to
+kubectl describe service/jenkins-svc | grep IP: | awk '{print $2;}'
+
+
+
+
+
+ 
