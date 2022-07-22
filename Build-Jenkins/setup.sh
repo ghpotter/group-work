3c319345b28a9756390086827d84583ee66352bf
tristan-genspark <tristan.stringfellow@genspark.net>
2022-07-21 19:09:18 -0500

    Updating

--- Build-Jenkins/setup.sh
+++ Build-Jenkins/setup.sh
@@ -1,7 +1,6 @@
 
 kubectl create namespace management-ns
-kubectl run jenkins --image=jenkins -n management-ns --restart=Never -o yaml > deploy.yaml
 
-kubectl apply -f deployment.yaml
+kubectl apply -f deploy.yaml
 
-kubectl expose deployment jenkins ---port=8080 --target-port=8080 --type=NodePort
+kubectl expose jenkins ---port=8080 --target-port=8080 --type=NodePort
